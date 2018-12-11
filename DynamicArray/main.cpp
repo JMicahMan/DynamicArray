b9d87b141133089f92dd687a272c245f8fad7ec9
JMicahMan <j.micah.manzanares@gmail.com>
2018-12-11 14:28:45 -0600

    Finished

--- DynamicArray/main.cpp
+++ DynamicArray/main.cpp
@@ -4,7 +4,9 @@ int main()
 {
 	DynamicArray<int>* testArray = new DynamicArray<int>();
 
-	
+	//insert adds a value to the array right after the previous value at the end of the array
+	// the resualt should be an array with values 5 10 15 4 4 9 4 115 14 35 84 90
+
 	testArray->insert(5);
 	testArray->insert(10);
 	testArray->insert(15);
@@ -17,12 +19,26 @@ int main()
 	testArray->insert(35);
 	testArray->insert(84);
 	testArray->insert(90);
+	//insert works as intended
+
 	testArray->print();
+	// prints the array to console for debugging
+
 	testArray->clear();
+	// clears the previously printed array for easier debugging
+
+	// remove should remove the the value at the end of the array
+	// the resault should be an array with values 5 10 15 4 4 9 4 115 14 35 84
+
 	testArray->remove();
+	// remove works as intended
 	testArray->print();
 	testArray->clear();
+
+	// sort should sort the array by least to greatest numerical values
+	// the resault should be an array with values 4 4 4 5 9 10 14 15 35 84 115
 	testArray->sort();
+	// sort works as intended
 	testArray->print();
 
 
